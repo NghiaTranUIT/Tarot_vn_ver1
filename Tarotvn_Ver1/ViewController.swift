5e94f074ec8b5fa0d6b19a74dbe39260d6bb6257
nhan92 <nhannt92@gmail.com>
2015-08-17 10:15:39 +0700

    Initial Commit

---
+++ Tarotvn_Ver1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarotvn_Ver1
+//
+//  Created by Nhan Nguyen on 8/17/15.
+//  Copyright (c) 2015 Nhan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
